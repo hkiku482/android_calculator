8c06e295ac0a9c045fc4e0ae635f20c32debd82d
H-Kiku482 <h.kikuchi482@gmail.com>
2022-06-29 17:37:15 +0900

    Update Calculator.kt

--- app/src/main/java/com/example/androidcalculator/Calculator.kt
+++ app/src/main/java/com/example/androidcalculator/Calculator.kt
@@ -1,4 +1,59 @@
 package com.example.androidcalculator
 
 class Calculator {
+    enum class State{
+        READY, NUM, SYM
+    }
+
+    private var currentInput: String = ""
+    private var formula: String = ""
+    private var state: State = State.READY
+
+    fun putRune(c: Char) {
+        if (c.code in 48..57) {
+//          (c: Char) is 0-9
+//          if user input 0 -> 0...
+            if (currentInput == "" && c == '0') {
+                return
+            }
+            state = State.NUM
+            currentInput += c.toString()
+        } else if (c == '.') {
+//          (C: Char) is dot
+            state = State.NUM
+            if (currentInput == "") {
+                currentInput = "0."
+            } else if (currentInput.indexOf('.') == -1){
+                currentInput += "."
+            }
+        } else if (c == '=') {
+            formula += currentInput
+            currentInput = ""
+        } else {
+//          any symbol has been inputted
+            when (state) {
+                State.NUM -> {
+                    state = State.SYM
+                    formula += currentInput
+                    formula += " $c "
+                }
+                State.SYM -> {
+                    formula = formula.dropLast(2) + "$c "
+                }
+                else -> {
+                    formula = "0 $c "
+                }
+            }
+            currentInput = ""
+        }
+    }
+
+    fun getFormula(): String {
+        return formula
+    }
+
+    fun getCurrentInput(): String {
+        return currentInput
+    }
+
 }
--- app/src/test/java/com/example/androidcalculator/ExampleUnitTest.kt
+++ app/src/test/java/com/example/androidcalculator/ExampleUnitTest.kt
@@ -11,7 +11,25 @@ import org.junit.Assert.*
  */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun calculatorInput() {
+        val calc1 = Calculator()
+        calc1.putRune('0')
+        calc1.putRune('0')
+        assertEquals(calc1.getCurrentInput(), "")
+        calc1.putRune('.')
+        calc1.putRune('0')
+        assertEquals(calc1.getCurrentInput(), "0.0")
+        calc1.putRune('1')
+        calc1.putRune('+')
+        assertEquals(calc1.getFormula(), "0.01 + ")
+        calc1.putRune('*')
+        assertEquals(calc1.getFormula(), "0.01 * ")
+        calc1.putRune('9')
+        calc1.putRune('=')
+        assertEquals(calc1.getFormula(), "0.01 * 9")
+
+        val calc2 = Calculator()
+        calc2.putRune('+')
+        assertEquals(calc2.getFormula(), "0 + ")
     }
 }
