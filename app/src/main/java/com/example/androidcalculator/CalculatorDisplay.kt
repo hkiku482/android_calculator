d062f021b35de076f0a5135773f0726ed1925cfd
H-Kiku482 <h.kikuchi482@gmail.com>
2022-09-13 01:07:26 +0900

    maximum primaryDisplay length

--- app/src/main/java/com/example/androidcalculator/CalculatorDisplay.kt
+++ app/src/main/java/com/example/androidcalculator/CalculatorDisplay.kt
@@ -14,11 +14,14 @@ class CalculatorDisplay(context: Context): CalculatorPresenter {
 
     override fun setPrimaryDisplay(status: String) {
         this.primaryDisplay = status
+        if (this.primaryDisplay.length > 10) {
+            this.primaryDisplay = this.primaryDisplay.substring(0,10)
+        }
         (context as MainActivity).findViewById<TextView>(R.id.primaryView).text = this.primaryDisplay
     }
 
     override fun setFormulaDisplay(formula: String) {
         this.formulaDisplay = formula
-        (context as MainActivity).findViewById<TextView>(R.id.secondaryView).text = this.primaryDisplay
+        (context as MainActivity).findViewById<TextView>(R.id.secondaryView).text = this.formulaDisplay
     }
 }
